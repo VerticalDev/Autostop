3d1d139beb8567c24e01bbca5a2b1ce0e935d28c
MathieuBarbier <m.barbier@vertic.al>
2015-03-14 00:28:44 +0100

    Chivas Woooops

--- Assets/Scripts/Model/Car.cs
+++ Assets/Scripts/Model/Car.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class Car : MonoBehaviour {
 
 	List<Passenger> passengers = new List<Passenger>();
-	public List<Dialog> dialogs = new LinkedList<Dialog>(); 
+	public List<Dialog> dialogs = new List<Dialog>(); 
 
 	// Use this for initialization
 	void Start () {
