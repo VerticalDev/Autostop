6d7e4ac9cf27c2f7756dab592833f1957760bf7d
BenouKat <ben.goze@gmail.com>
2015-03-13 22:34:33 +0100

    GameManager and Utils

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	void Awake()
+	{
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad(this);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Utils/DestroyThisTimed.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyThisTimed : MonoBehaviour {
+
+
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, time);
+	}
+}
