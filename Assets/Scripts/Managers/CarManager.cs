b35d981215c873d6a83a70606d224d863ea715ba
MathieuBarbier <m.barbier@vertic.al>
2015-03-14 00:28:16 +0100

    Chivas l'essence de la vie

---
+++ Assets/Scripts/Managers/CarManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarManager : MonoBehaviour {
+
+	public static CarManager instance;	
+	void Awake()	{		if (instance == null) {			instance = this;		DontDestroyOnLoad(this);		}	}
+
+	List<Car> cars = new List<Car>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Model/Car.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Car : MonoBehaviour {
+
+	List<Passenger> passengers = new List<Passenger>();
+	public List<Dialog> dialogs = new LinkedList<Dialog>(); 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
