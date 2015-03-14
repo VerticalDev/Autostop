754cb1a9824e4f3310d01117d89a2ff4f54bddd0
MathieuBarbier <m.barbier@vertic.al>
2015-03-14 22:52:05 +0100

    Chivas du roadsystem

---
+++ Assets/Scripts/Managers/RoadSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoadSystem : MonoBehaviour {
+
+	public List<GameObject> props;
+	public float propInterval = 5f;
+	public Transform propSpawnLoc;
+	public float citySpawnProbability = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("createProp", propInterval,propInterval);
+	}
+
+	public void createProp(){
+		if(Random.value<citySpawnProbability)
+			Instantiate (props [Random.Range (0, props.Count+1)], propSpawnLoc.position, Quaternion.identity);
+	}
+
+}
