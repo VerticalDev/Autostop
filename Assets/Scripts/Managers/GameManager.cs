51fbee0c08d425e1e0808b432e505a480e4365b9
MathieuBarbier <m.barbier@vertic.al>
2015-03-14 12:08:20 +0100

    Chivas de la scene

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
 	void Awake(){if (instance == null) {instance = this;DontDestroyOnLoad(this);}}
 
+	AsyncOperation async = new AsyncOperation();
+
+	public enum carType {
+		van,
+		volvo,
+		caravan,
+		cadillac
+	}
+
+	public enum roadType {
+		summer,
+		summer2,
+		winter
+	}
+
+	public int carsTaken = 0; 
+
 	// Use this for initialization
 	void Start () {
-		//Application.LoadLevel ("RoadScene");
+		gotoRoad (roadType.summer);
 	}
-	
+
+	public void gotoRoad(roadType type){
+		async = Application.LoadLevelAsync ("Road" + type.ToString ());
+	}
+
+	public void gotoCar(carType type){
+		async = Application.LoadLevelAsync ("Car" + type.ToString ());
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+		if (async.progress > 0 && async.progress < 1) {
+			Debug.Log("Loadin'");
+		}
 	}
 }
